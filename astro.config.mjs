6a665c1710e529c9d27eceb8bf24b510823e11be
mikequeve <michaelvega46@gmail.com>
2024-10-04 14:45:19 -0600

    Agregando google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ['dataKayer.push'],
+      },
+    }),
+  ],
 });
